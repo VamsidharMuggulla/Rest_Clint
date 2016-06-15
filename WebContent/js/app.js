486e1f35690a895b32d94209577a9fafc5af8e3a
Vamsidhar Muggulla <vam.vamshe@gmail.com>
2016-06-15 16:53:03 +0530

    Update app.js

--- WebContent/js/app.js
+++ WebContent/js/app.js
@@ -1,115 +1,149 @@
 (function() {
 
-	var app=angular.module('Contacts',[]);
-	
-	app.controller('ContactsController', function($http){
-	    var cont = this;
-	    $http.get('http://localhost:8080/RestClint/handlethose/peoples/contacts/get')
-	    .success(function(response) {
-	    	cont.contact=response;
-	       console.log(response);	
-	    });
-	   /*
-	    cont.contact=[
-	           	  {
-	           	    "name": "Santa",
-	           	    "number": "9177595206"
-	           	  },
-	           	  {
-	           		    "name": "Baby",
-	           		    "number": "987795206"
-	           	  },
-	           	{
-	           		    "name": "Royale",
-	           		    "number": "9876543214"
-	           	  },
-	           	{
-	           		    "name": "Spin",
-	           		    "number": "9856314756"
-	           	  }
-	           	];
+	var app = angular.module('Contacts', []);
 
-*/	    
-	  });
-	app.controller('ContactAddController',function() {
-		
-		this.con={};
-		this.addContact=function(cntact){
-			cntact.push(this.con);
-			console.log(this.con.name+"ADD");
-			console.log(this.con.number+"ADD");
-			this.con={};	
-		};
-		
-	});	
-	app.controller('DeleteController',function() {
-		this.con={};
-		this.deleteContact=function(cntact) {
+	app
+			.controller(
+					'ContactsController',
+					function($http) {
+						var cont = this;
+						$http
+								.get(
+										'http://localhost:8080/RestClint/handlethose/peoples/contacts/get')
+								.success(function(response) {
+									if (response.length == 0) {
+										/* cont.contact=[{}]; */
 
-			console.log("absc");
-			cntact.pop(this.con);
-		};
-	});
-	
-	
-	app.controller('UpdateController',function() {
-		this.con={};
-		
-			this.updateContact=function(cntact) {
-				var na=document.getElementById("namee").value;
-				
-				var num=document.getElementById("numbere").value;
-				for(i=0;i<cntact.length;i++) {
-					console.log(cntact[i].name+"UP");
-					if(cntact[i].name==na) {
-						console.log(cntact[i].name+"UP");
-						cntact[i].name=na;
-						cntact[i].number=num;					
-						
-					}
-				}
-			};
-		
-	});
+										console.log("IFFF");
+									} else {
+										cont.contact = response;
+										console.log("Else");
+									}
+									console.log(response);
+								});
+
+						/*
+						 * cont.contact=[ { "name": "Santa", "number":
+						 * "9177595206" }, { "name": "Baby", "number":
+						 * "987795206" }, { "name": "Royale", "number":
+						 * "9876543214" }, { "name": "Spin", "number":
+						 * "9856314756" } ];
+						 * 
+						 */
+					});
+	app
+			.controller(
+					'ContactAddController',
+					function($http) {
+
+						this.con = {};
+						this.addContact = function(cntact) {
+							/*
+							 * cntact.push(this.con);
+							 * console.log(this.con.name+"ADD");
+							 * console.log(this.con.number+"ADD"); this.con={};
+							 */
+							$http
+									.post(
+											'http://localhost:8080/RestClint/handlethose/peoples/contacts/post',
+											{}, {
+												params : {
+													name : this.con.name,
+													number : this.con.number
+												}
+											}).success(function(response) {
+
+										console.log(response);
+									});
+							console.log(this.con);
+							if (cntact.length == 0) {
+
+								/*
+								 * cntact=[{}]; cntact.pop();
+								 */
+								cntact.push(this.con);
+								console.log("IF" + cntact);
+								this.con = {};
+							} else {
+								cntact.push(this.con);
+								this.con = {};
+								console.log("ELSE" + cntact);
+							}
+						};
+
+					});
+	app
+			.controller(
+					'DeleteController',
+					function($http) {
+						this.con = {};
+						this.deleteContact = function(cntact) {
+
+							if (!cntact.length == 0) {
+								var o = cntact.pop(this.con);
+								var num = o.number;
+								var na = o.name;
+
+								$http(
+										{
+											method : 'DELETE',
+											url : 'http://localhost:8080/RestClint/handlethose/peoples/contacts/delete',
+											params : {
+												name : na,
+												number : num
+											}
+										}).then(function(response) {
+
+									console.log(response);
+								});
+							} else {
+
+								alert("Contacts Deleted!!");
+
+							}
+						};
+					});
+
+	app
+			.controller(
+					'UpdateController',
+					function($scope, $http) {
+						this.con = {};
+
+						this.updateContact = function(cntact) {
+							var na = document.getElementById("namee").value;
+							var num = document.getElementById("numbere").value;
+
+							$http
+									.put(
+											'http://localhost:8080/RestClint/handlethose/peoples/contacts/put',
+											{}, {
+												params : {
+													name : na,
+													number : num
+												}
+											})
+									.success(
+											function(response) {
+												for (i = 0; i < cntact.length; i++) {
+													console.log(cntact[i].name
+															+ "UP");
+													if (cntact[i].name == na) {
+														console
+																.log(cntact[i].name
+																		+ "UP");
+														cntact[i].name = na;
+														cntact[i].number = num;
+
+													}
+												}
+												console.log(response);
+												//alert(response);
+												//$scope.mess=response;
+											});
+
+						};
+
+					});
 
 })();
-
-
-
-/*
- 
-	app.controller('UpdateController',['$scope',function($scope) {
-		$scope.updateCtrl.con=[];
-		$scope.updateCtrl.con.number;
-		$scope.updateCtrl.con.name;
-		$scope.$watch("updateCtrl.con.name","updateCtrl.con.number",function(newVal,oldVal) {
-			if($scope.updateCtrl.con.name!="" && $scope.updateCtrl.con.number!="")
-				{
-					console.log($scope.updateCtrl.con.name);
-					console.log($scope.updateCtrl.con.number);
-				}
-			else
-				{	
-				console.log($scope.updateCtrl.con.name);
-				console.log($scope.updateCtrl.con.number);
-				
-				}
-		},true);
-		this.updateContact=function(cntact) {
-			
-			
-			
-			for(i=0;i<cntact.length;i++) {
-				if(cntact[i].name==$scope.updateCtrl.con.name) {
-					cntact[i].number=$scope.updateCtrl.con.number;
-				}
-			}
-			console.log($scope.updateCtrl.con.name);
-			console.log($scope.updateCtrl.con.number);
-		
-			console.log(cntact);
-			console.log(this.con);
-			//this.con={};
-		};
-	}]);
-*/
